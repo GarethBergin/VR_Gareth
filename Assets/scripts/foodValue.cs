d2c5b08f86aea7aa08fdf17d9cee5b62fc7e67bf
GarethBergin <garethbergin2003@gmail.com>
2024-11-16 17:44:01 +0000

    borger

---
+++ Assets/scripts/foodValue.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodValue : MonoBehaviour
+{
+    public Transform cloneObj;
+    public int foodValues;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
